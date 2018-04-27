7bc9ced68a0fea628b36a6deb761a5ab6ebf9d09
Ash <quarktheawesome@gmail.com>
2018-04-27 20:19:21 +1000

    cafe/dmae: Add some Doxygen documentation

--- src/libdecaf/src/modules/dmae/dmae_core.cpp
+++ src/libdecaf/src/modules/dmae/dmae_core.cpp
@@ -7,10 +7,24 @@
 namespace dmae
 {
 
+/**
+ * \defgroup dmae dmae
+ *
+ * Functions to interact with the Espresso's DMA engine and perform accelerated
+ * memory operations.
+ * @{
+ */
+
+/**
+ * Possible endian swap methods to apply during a copy.
+ */ 
 enum class DMAEEndianSwapMode : uint32_t
 {
+   //! No endian manipulation is performed.
    None = 0,
+   //! Divide input into 16-bit values, then perform an endian swap on each.
    Swap8In16 = 1,
+   //! Divide input into 32-bit values, then perform an endian swap on each.
    Swap8In32 = 2
 };
 
@@ -20,24 +34,81 @@ sDmaeTimeout = 5;
 static coreinit::OSTime
 sLastTimeStamp = 0;
 
+/**
+ * Gets the last retired operation.
+ *
+ * \return
+ * The timestamp corresponding to the last retired operation.
+ *
+ * \if false
+ * meta: what is this actually supposed to do? would like to write something
+ * slightly more helpful
+ * \endif
+ */
 coreinit::OSTime
 DMAEGetRetiredTimeStamp()
 {
    return sLastTimeStamp;
 }
 
+/**
+ * Gets the operation last submitted to the hardware.
+ *
+ * \return
+ * The timestamp corresponding to the last submitted operation.
+ */
 coreinit::OSTime
 DMAEGetLastSubmittedTimeStamp()
 {
    return sLastTimeStamp;
 }
 
+/**
+ * Wait for a given operation to complete.
+ *
+ * \param timestamp
+ * The timestamp corresponding to the operation to wait for.
+ *
+ * \return
+ * \c true if the operation completed successfully.
+ *
+ * \if false
+ * meta: made up the return bit, please correct if wrong. perhaps false means a
+ * timeout occured?
+ * \endif 
+ */
 BOOL
 DMAEWaitDone(coreinit::OSTime timestamp)
 {
    return TRUE;
 }
 
+/**
+ * Start an async copy operation using the hardware DMA engine.
+ * When using DMAEEndianSwapMode::None, this function is similar to memcpy.
+ * This method can be significantly faster than CPU-based copies, and can be
+ * processed asynchronously.
+ *
+ * \param dst
+ * The destination's virtual address.
+ *
+ * \param src
+ * The source's virtual address.
+ *
+ * \param numDwords
+ * The size of the copy operation *in dwords* (32-bit/4-byte groups)
+ *
+ * \param endian
+ * The type of endian swapping to use while copying.
+ *
+ * \return
+ * A timestamp corresponding with the copy. Used with other DMAE functions to
+ * identify this operation.
+ *
+ * \sa
+ *  - DMAEWaitDone()
+ *  - DMAEFillMem()
+ */
 coreinit::OSTime
 DMAECopyMem(void *dst, void *src, uint32_t numDwords, DMAEEndianSwapMode endian)
 {
@@ -61,6 +132,29 @@ DMAECopyMem(void *dst, void *src, uint32_t numDwords, DMAEEndianSwapMode endian)
    return sLastTimeStamp;
 }
 
+/**
+ * Start an async fill operation using the hardware DMA engine.
+ * This function is similar to memset.
+ * This method can be significantly faster than CPU-based fills, and can be
+ * processed asynchronously.
+ *
+ * \param dst
+ * The destination's virtual address.
+ *
+ * \param value
+ * The 32-bit value to fill the memory with.
+ *
+ * \param numDwords
+ * The size of the fill operation *in dwords* (32-bit/4-byte groups)
+ *
+ * \return
+ * A timestamp corresponding with the fill. Used with other DMAE functions to
+ * identify this operation.
+ *
+ * \sa
+ *  - DMAEWaitDone()
+ *  - DMAECopyMem()
+ */
 coreinit::OSTime
 DMAEFillMem(void *dst, uint32_t value, uint32_t numDwords)
 {
@@ -76,17 +170,41 @@ DMAEFillMem(void *dst, uint32_t value, uint32_t numDwords)
    return sLastTimeStamp;
 }
 
+/**
+ * Sets the timeout for DMA operations.
+ *
+ * \param timeout
+ * The new timeout to set.
+ *
+ * \sa
+ * - DMAEGetTimeout()
+ *
+ * \if false
+ * meta: what unit is this in? what does it affect?
+ * \endif
+ */
 void
 DMAESetTimeout(uint32_t timeout)
 {
    sDmaeTimeout = timeout;
 }
 
+/**
+ * Gets the current timeout for DMA operations.
+ *
+ * \return
+ * The current timeout.
+ *
+ * \sa
+ * - DMAESetTimeout()
+ */
 uint32_t
 DMAEGetTimeout()
 {
    return sDmaeTimeout;
 }
+
+/** @} */
 
 void
 Module::registerCoreFunctions()
